cb8dc5001c97096f3df8e07e434fcd4530ccdab9
ronaks9065 <115705180+ronaks9065@users.noreply.github.com>
2024-12-06 13:29:42 +0530

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -7,34 +7,11 @@ AWS_REGION="eu-central-1"
 ECR_REPOSITORY="daps"
 IMAGE_TAG="latest"
 AWS_ACCOUNT_ID="559050212190"
-DOCKERFILE_CONTENT="""
-ARG KC_VERSION=24.0.3
-
-FROM maven:3-eclipse-temurin-17 AS daps-ext-builder
-ARG KC_VERSION
-
-WORKDIR /home/app
-COPY . ./
-#RUN --mount=type=cache,target=/root/.m2 mvn -D \"version.keycloak=${KC_VERSION}\" clean package
-RUN mvn -D \"version.keycloak=${KC_VERSION}\" clean package
-
-FROM quay.io/keycloak/keycloak:${KC_VERSION}
-COPY --from=daps-ext-builder /home/app/target/dat-extension.jar /opt/keycloak/providers/dat-extension.jar
-
-# Theme Customization
-COPY themes/ /opt/keycloak/themes/
-
-CMD [\"start\"]
-"""
 
 # Step 1: Clone the repository
 echo "Cloning the repository..."
 git clone $REPO_URL
 cd $CLONE_DIR || exit 1
-
-# Step 2: Replace Dockerfile with the provided one
-echo "Replacing Dockerfile..."
-echo "$DOCKERFILE_CONTENT" > Dockerfile
 
 # Step 3: Authenticate with AWS ECR
 echo "Authenticating with AWS ECR..."
@@ -42,7 +19,7 @@ aws ecr get-login-password --region eu-central-1 | docker login --username AWS -
 
 # Step 4: Build the Docker image
 echo "Building the Docker image..."
-docker build -t "${ECR_REPOSITORY}:${IMAGE_TAG}" .
+docker build -f Dockerfile -t "${ECR_REPOSITORY}:${IMAGE_TAG}" .
 
 # Step 5: Push the Docker image to ECR
 echo "Pushing the Docker image to ECR..."
